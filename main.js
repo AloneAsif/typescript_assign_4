dbc0403f1d9122a7ef182754319a6d5aec262b3c
AloneAsif <70218962+AloneAsif@users.noreply.github.com>
2024-03-06 20:07:03 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,1 @@
+console.log('Ali Ibn Abi Talib, "The disease of the heart is worse than the disease of the body."');
---
+++ main.ts
@@ -0,0 +1,4 @@
+
+
+console.log('Ali Ibn Abi Talib, "The disease of the heart is worse than the disease of the body."');
+
